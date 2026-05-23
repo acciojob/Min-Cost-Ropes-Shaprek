0bf8a6679371092048b109de064b7bebd5ed54d6
Preksha_jain <134912966+Shaprek@users.noreply.github.com>
2026-05-24 00:40:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function mincost(arr)
 { 
 //write your code here
-// return the min cost
+// return the min cost'
+	arr.map(Number);
+	arr.sort((a,b)=>a-b);
+	let sum=0;
+	arr.map((elem, index)=>{
+		if(index==0)sum*(arr.length-1);
+		else sum*(arr.length-index);
+	});
+	return sum;
   
 }
 
